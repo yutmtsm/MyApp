a90bcc67dfc719500dca5b08e1538281ebddd1d4
”yutmtsm” <yutmtsm@gmail.com>
2020-08-11 01:38:46 +0000

    オリジナルアプリ作成1周目

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title')</title>
+        
+        <!-- asset(‘ファイルパス’)は、publicディレクトリのパスを返す関数 -->
+        <!-- Scripts -->
+         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/top.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            {{-- 画面上部に表示するナビゲーションバーです。 --}}
+            <!--<nav class="navbar navbar-expand-md navbar-dark navbar-laravel">-->
+            <!--    <div class="container">-->
+            <!--        <a class="navbar-brand" href="{{ url('/') }}">-->
+            <!--            {{ config('app.name', 'Laravel') }}-->
+            <!--        </a>-->
+            <!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+            <!--            <span class="navbar-toggler-icon"></span>-->
+            <!--        </button>-->
+
+            <!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+            <!--             Left Side Of Navbar -->
+            <!--            <ul class="navbar-nav mr-auto">-->
+
+            <!--            </ul>-->
+
+            <!--             Right Side Of Navbar -->
+            <!--            <ul class="navbar-nav ml-auto">-->
+            <!--            </ul>-->
+            <!--        </div>-->
+            <!--    </div>-->
+            <!--</nav>-->
+            
+            <!--<header>-->
+            <!--    <div class="container">-->
+            <!--        <div class="header-left">-->
+            <!--            <h2>MoneyBike</h2>-->
+            <!--        </div>-->
+            <!--        <div class="header-right">-->
+            <!--            <ul>-->
+            <!--                <li><a class="login" href="#">HOME</a></li>-->
+            <!--                <li><a class="login" href="#">ABOUT</a></li>-->
+            <!--                <li><a class="login" href="#">SERVICE</a></li>-->
+            <!--                <li><a class="login" href="#">新規登録</a></li>-->
+            <!--                <li><a class="login" href="#">ログイン</a></li>-->
+            <!--            </ul>-->
+            <!--        </div>-->
+            <!--    </div>-->
+            <!--</header>-->
+            
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel" id="navbarSupportedContent">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'MoneyBike') }}
+                    </a>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="header-right callapse navbar-collapse">
+                        <ul class="navbar-nav ml-auto">
+                            <li><a class="login" href="#">HOME</a></li>
+                            <li><a class="login" href="#">ABOUT</a></li>
+                            <li><a class="login" href="#">SERVICE</a></li>
+                            <li><a class="login" href="#">新規登録</a></li>
+                            <li><a class="login" href="#">ログイン</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+            <footer>
+                
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.index')
+
+@section('title', 'TOP')
+
+@section('content')
+    <div class="container">
+        <div class="row align-items-stretch retro-layout-2">
+          <div class="col-md-6">
+            <a href="#" class="h-entry mb-30 v-height gradient" style="height: 450px; width: 450px;">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+            <a href="#" class="h-entry mb-30 v-height gradient">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+            <a href="#" class="h-entry mb-30 v-height gradient">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6">
+            <a href="#" class="article-box h-entry mb-30 v-height gradient">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+            <a href="#" class="h-entry mb-30 v-height gradient">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+            <a href="#" class="h-entry mb-30 v-height gradient">
+              
+              <div class="">
+                <h2>ここにに記事の画像をランダム</h2>
+                <span class="date">ここに投稿日July 19, 2019</span>
+              </div>
+            </a>
+          </div>
+        </div>
+    </div>
+@endsection
