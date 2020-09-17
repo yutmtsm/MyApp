80c76743721f5edcc64593b80c6a4ee437bac156
”yutmtsm” <yutmtsm@gmail.com>
2020-09-17 11:18:46 +0000

    original App

--- resources/views/admin/post/detail.blade.php
+++ resources/views/admin/post/detail.blade.php
@@ -41,100 +41,113 @@
                 <label class="post-comment"></label>
                 <p style="color: white;">{{ $post->comment }}</p>
             </div>
-            <!-- スポット -->
-            <div class="form-group">
-                <label class="control-label">スポット</label>
-                <!--<input type="text" class="form-control" name="spot" value="{{ old('spot') }}">-->
-                <p style="color: white;">{{ $post->spot }}</p>
-            </div>
-            <!-- 使用金 -->
-            <div class="d-flex align-items-center">
-                <div class="form-group">
-                    <label class="control-label">施設費</label>
-                    <!--<input type="text" class="form-control" name="addmission_fee" value="{{ old('addmission_fee') }}">-->
-                    <p style="color: white;">{{ number_format($post->addmission_fee) }}円</p>
-                </div>
-                <div class="form-group" style="margin-left: 100px;">
-                    <label class="control-label">購入金</label>
-                    <!--<input type="text" class="form-control" name="purchase_cost" value="{{ old('purchase_cost') }}">-->
-                    <p style="color: white;">{{ number_format($post->purchase_cost) }}円</p>
-                </div>
-                <div class="form-group" style="margin-left: 100px;">
-                    <label class="control-label">トータル</label>
-                    <!--<input type="text" class="form-control" name="purchase_cost" value="{{ old('purchase_cost') }}">-->
-                    <p style="color: white;">{{ number_format($total_cost) }}円</p>
-                </div>
-            </div>
-            <!-- 画像 -->
-            <div class="form-group">
-                @if(isset($post->image_path))
-                <img width="300px" height="auto" src="/storage/image/post/{{ $post->image_path }}">
-                @else
-                <img width="300px" height="auto" style="margin-bottom: 5px;" src="/storage/image/noimage.png">
-                @endif
-            </div>
-            <div class="col-md-6 comment-btn">
-                <!-- コメント表示モーダル -->
-                <button type="button" class="bike-detail-btn btn w-10 h-25" style="padding: 0;" data-toggle="modal" data-target="#exampleModal{{ $post->id }}">
-                    <p class="text-right btn btn-primary">この投稿にコメント</p>
-                </button>
-                
-                <!-- Modal -->
-                <div class="modal fade bd-example-modal-lg" id="exampleModal{{ $post->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModal3Label" aria-hidden="true">
-                  <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModal3Label">
-                                <div class="col-md-12 d-flex no-gutters text-secondary">
-                                    <div>
-                                        @if(isset($post->image_icon))
-                                       <img class="post-icon" src="/storage/image/user/{{ $post->image_icon }}">
-                                        @else
-                                        <img class="post-icon" src="/storage/image/noimage.png">
-                                        @endif
-                                        <div class="v_line_fix" style="margin:5px 0 5px 25px;width: 2px;height: 50px;background-color: brown;margin:"></div>
-                                    </div>
-                                   <div class="post-top">
-                                         <div class="form-inline">
-                                             <div class="post-name" style="margin-right: 10px;">{{ $post->user_name }}</div>
-                                           <div class="post-date">{{ $post->created_at }}</div>
-                                        </div>
-                                        <div class="post-title">
-                                            <h5>『{{ $post->title }}』</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12 d-flex no-gutters text-secondary">
-                                    <div>
-                                        @if(isset($post->image_icon))
-                                       <img class="post-icon" src="/storage/image/user/{{ $user->image_path }}">
-                                        @else
-                                        <img class="post-icon" src="/storage/image/noimage.png">
-                                        @endif
-                                    </div>
-                                    <form action="{{ action('Admin\CommentController@create', ['id' => $post->id]) }}" method="post">
-                                        <div class="form-group">
-                                            <textarea class="form-control" name="comment" value="{{ old('comment') }}" style="resize: none;width:450px;height:200px;" placeholder="思ったことや気になることなどを入力してください">{{ old('comment') }}</textarea>
-                                            {{ csrf_field() }}
-                                            <input type="submit" class="btn-primary add-btn" value="コメント">
-                                        </div>
-                                    </form>
-                                </div>
-                            </h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                <!-- スポット -->
+            <div class="d-flex">
+                <div class="col-md-7">
+                    <div class="form-group">
+                        <label class="control-label">スポット</label>
+                        <!--<input type="text" class="form-control" name="spot" value="{{ old('spot') }}">-->
+                        <p style="color: white;">{{ $post->spot }}</p>
+                    </div>
+            
+                    <!-- 使用金 -->
+                    <div class="d-flex align-items-center">
+                        <div class="form-group">
+                            <label class="control-label">施設費</label>
+                            <!--<input type="text" class="form-control" name="addmission_fee" value="{{ old('addmission_fee') }}">-->
+                            <p style="color: white;">{{ number_format($post->addmission_fee) }}円</p>
                         </div>
-                        <div class="modal-body">
-                            
+                        <div class="form-group" style="margin-left: 100px;">
+                            <label class="control-label">購入金</label>
+                            <!--<input type="text" class="form-control" name="purchase_cost" value="{{ old('purchase_cost') }}">-->
+                            <p style="color: white;">{{ number_format($post->purchase_cost) }}円</p>
                         </div>
-                        <div class="modal-footer">
-                            
+                        <div class="form-group" style="margin-left: 100px;">
+                            <label class="control-label">トータル</label>
+                            <!--<input type="text" class="form-control" name="purchase_cost" value="{{ old('purchase_cost') }}">-->
+                            <p style="color: white;">{{ number_format($total_cost) }}円</p>
                         </div>
                     </div>
-                  </div>
+                    <!-- 画像 -->
+                    <div class="form-group">
+                        @if(isset($post->image_path))
+                        <img width="300px" height="auto" src="/storage/image/post/{{ $post->image_path }}">
+                        @else
+                        <img width="300px" height="auto" style="margin-bottom: 5px;" src="/storage/image/noimage.png">
+                        @endif
+                    </div>
+                    <div class="col-md-6 comment-btn">
+                    <!-- コメント表示モーダル -->
+                        <button type="button" class="bike-detail-btn btn w-10 h-25" style="padding: 0;" data-toggle="modal" data-target="#exampleModal{{ $post->id }}">
+                            <p class="text-right btn btn-primary">この投稿にコメント</p>
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade bd-example-modal-lg" id="exampleModal{{ $post->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModal3Label" aria-hidden="true">
+                          <div class="modal-dialog modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModal3Label">
+                                        <div class="col-md-12 d-flex no-gutters text-secondary">
+                                            <div>
+                                                @if(isset($post->image_icon))
+                                               <img class="post-icon" src="/storage/image/user/{{ $post->image_icon }}">
+                                                @else
+                                                <img class="post-icon" src="/storage/image/noimage.png">
+                                                @endif
+                                                <div class="v_line_fix" style="margin:5px 0 5px 25px;width: 2px;height: 50px;background-color: brown;margin:"></div>
+                                            </div>
+                                           <div class="post-top">
+                                                 <div class="form-inline">
+                                                     <div class="post-name" style="margin-right: 10px;">{{ $post->user_name }}</div>
+                                                   <div class="post-date">{{ $post->created_at }}</div>
+                                                </div>
+                                                <div class="post-title">
+                                                    <h5>『{{ $post->title }}』</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 d-flex no-gutters text-secondary">
+                                            <div>
+                                                @if(isset($post->image_icon))
+                                               <img class="post-icon" src="/storage/image/user/{{ $user->image_path }}">
+                                                @else
+                                                <img class="post-icon" src="/storage/image/noimage.png">
+                                                @endif
+                                            </div>
+                                            <form action="{{ action('Admin\CommentController@create', ['id' => $post->id]) }}" method="post">
+                                                <div class="form-group">
+                                                    <textarea class="form-control" name="comment" value="{{ old('comment') }}" style="resize: none;width:450px;height:200px;" placeholder="思ったことや気になることなどを入力してください">{{ old('comment') }}</textarea>
+                                                    {{ csrf_field() }}
+                                                    <input type="submit" class="btn-primary add-btn" value="コメント">
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    
+                                </div>
+                                <div class="modal-footer">
+                                    
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                    </div>
+                    
+                    <!-- Modalここまで -->
+                    </div>
+                <div class="col-md-6">
+                    <iframe style="border: 0;" 
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDPv-uNVityorhO-YUuKfYxg7F5ab-NumM&q={{ $post->spot}}" 
+                    width="300" height="300" frameborder="0" allowfullscreen="allowfullscreen">
+                    </iframe>
                 </div>
-                <!-- Modalここまで -->
             </div>
         </div>
         <div class="col-md-3 mx-auto">
@@ -172,7 +185,7 @@
             </div>
             @endforeach
         </div>
-    </div>
+        
     
     
 </div>
